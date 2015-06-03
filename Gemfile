c64de9d524c66e0b811af38b3864aa115b4d57be
pankajagrawal <pankaj11.a@tcs.com>
2015-06-03 17:46:08 +0200

    3rd commit

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml-rails'
 gem 'awesome_print'
-
+gem 'bootstrap-sass'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150603153648) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "tittle"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
